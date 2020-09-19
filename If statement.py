c73e5ed844fb90c2505b7cb0f8c2b0f240eb6093
Huri <disatpathy@outlook.com>
2020-09-19 20:57:32 +0530

    :)

---
+++ If statement.py
@@ -0,0 +1,9 @@
+favanime = input("Is Gintama your fav anime ? True/False : ")
+
+if favanime == 'True':
+    print("Oh wow it's also my fav anime, I think we will become get friends :)")
+else:
+    favanime2 = input("Oh okay, Gintama is my fav anime tho. So tell me what's your fav anime then ? ")
+    print(favanime2 + " ! noice, that shit is actually good")
+
+# 1:54
